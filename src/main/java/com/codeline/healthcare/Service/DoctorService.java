34ed665ff22150a116fe47e80ed5bfe3b339a997
manal7h <manal.alsenani22@gmail.com>
2023-05-12 09:02:54 -0800

    create new doctor in doctorService class

--- src/main/java/com/codeline/healthcare/Service/DoctorService.java
+++ src/main/java/com/codeline/healthcare/Service/DoctorService.java
@@ -1,4 +1,25 @@
 package com.codeline.healthcare.Service;
 
+import com.codeline.healthcare.Model.Doctor;
+import com.codeline.healthcare.Repository.DoctorRepository;
+import com.codeline.healthcare.Request.DoctorRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DoctorService {
+
+    @Autowired
+    DoctorRepository doctorRepository;
+
+    //1.Create
+    public String createDoctor(DoctorRequest request) {
+        Doctor doctor = new Doctor();
+        doctor.setDoctorName(request.getDoctorName());
+        doctor.setDoctorEmail(request.getDoctorEmail());
+        doctor.setDoctorDepartment(request.getDoctorDepartment());
+        doctor.setDoctorSpecialization(request.getDoctorSpecialization());
+        doctorRepository.save(doctor);
+        return "Created Successfully";
+    }
 }
