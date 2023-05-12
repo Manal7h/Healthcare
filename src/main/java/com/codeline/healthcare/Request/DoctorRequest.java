abe5ae4a592badd0a4afacc9178a2cd25c381170
manal7h <manal.alsenani22@gmail.com>
2023-05-12 08:59:37 -0800

    add properities in class DoctorRequest

--- src/main/java/com/codeline/healthcare/Request/DoctorRequest.java
+++ src/main/java/com/codeline/healthcare/Request/DoctorRequest.java
@@ -1,4 +1,23 @@
 package com.codeline.healthcare.Request;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
 public class DoctorRequest {
+
+    Integer doctorId;
+
+    String doctorName;
+
+    String doctorEmail;
+
+    String doctorDepartment;
+
+    String doctorSpecialization;
+
+
 }
