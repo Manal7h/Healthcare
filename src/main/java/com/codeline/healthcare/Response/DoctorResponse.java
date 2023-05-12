f4c9c13c2e1876ee0613454703f4e340402a7ea6
manal7h <manal.alsenani22@gmail.com>
2023-05-12 09:49:11 -0800

    convert To Response from Doctor to DoctorResponse

--- src/main/java/com/codeline/healthcare/Response/DoctorResponse.java
+++ src/main/java/com/codeline/healthcare/Response/DoctorResponse.java
@@ -1,5 +1,7 @@
 package com.codeline.healthcare.Response;
 
+import com.codeline.healthcare.Model.Doctor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,6 +9,7 @@ import lombok.Setter;
 @Data
 @Getter
 @Setter
+@Builder
 public class DoctorResponse {
     Integer id;
 
@@ -17,4 +20,13 @@ public class DoctorResponse {
     String department;
 
     String specialization;
+
+    public static DoctorResponse convertToResponse(Doctor request) {
+        return DoctorResponse.builder()
+                .name(request.getDoctorName())
+                .email(request.getDoctorEmail())
+                .department(request.getDoctorDepartment())
+                .specialization(request.getDoctorSpecialization())
+                .build();
+    }
 }
